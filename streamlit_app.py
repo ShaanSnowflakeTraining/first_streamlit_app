19d59c0f4fd4077e31cfb5d58cbc9a5d0cdcf298
ShaanSnowflakeTraining <143792474+ShaanSnowflakeTraining@users.noreply.github.com>
2023-09-01 11:34:02 +0200

    Update streamlit_app.py
    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinage & Rocket Smoothie')
+streamlit.text(Hard boiled free range fair trade eggs')
