193cd29cca311722fac9f1ddb1cb7c43f945d794
ShaanSnowflakeTraining <143792474+ShaanSnowflakeTraining@users.noreply.github.com>
2023-09-02 16:49:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 ##Display table on page
 streamlit.dataframe(fruits_to_show)
 
-def get_fruitvice_data('this_fruit_choice'):
+def get_fruitvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 return fruityvice_normalized
